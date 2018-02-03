f0a4f3bd427b9cb7bf45bfe21bbef4e2c2c029c2
myleslambert <myleslambert18@gmail.com>
2018-02-03 16:08:08 +0100

    - WIP Curve Editor

---
+++ Assets/Plugins/BedrockFramework/CustomCurve.cs
@@ -0,0 +1,164 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using System;
+using System.Linq;
+
+namespace BedrockFramework.CustomLine
+{
+    public class CustomCurve : MonoBehaviour
+    {
+        [Serializable]
+        public struct CurvePoint
+        {
+            public Vector3 curvePoint;
+            public Vector3 curveTangent;
+
+            public CurvePoint(Vector3 pos, Vector3 tang)
+            {
+                curvePoint = pos;
+                curveTangent = tang;
+            }
+        }
+
+        public CurvePoint[] points;
+
+        public void Reset()
+        {
+            points = new CurvePoint[] {
+            new CurvePoint(new Vector3(1f, 0f, 0f), new Vector3(1f, 0f, 0f)),
+            new CurvePoint(new Vector3(5f, 0f, 0f), new Vector3(1f, 0f, 0f)),
+            };
+
+            cachedDistanceToT = null;
+        }
+
+        public int CurveCount
+        {
+            get
+            {
+                return (points.Length - 1);
+            }
+        }
+
+
+        private SortedList<float, float> cachedDistanceToT;
+        public void CacheDistanceToT(int stepsPerCurve = 10)
+        {
+            cachedDistanceToT = new SortedList<float, float>();
+
+            int numSteps = (stepsPerCurve * CurveCount);
+
+            float tStepSize = 1f / numSteps;
+            float length = 0;
+
+            Vector3 lastPos = Vector3.zero;
+            for (int i = 0; i < numSteps + 1; i++)
+            {
+                float t = i * tStepSize;
+
+                Vector3 iPos = GetPoint(t);
+
+                if (i != 0)
+                    length += Vector3.Distance(lastPos, iPos);
+                lastPos = iPos;
+
+                cachedDistanceToT[length] = t;
+            }
+        }
+
+        public float DistanceToT(float distace)
+        {
+            if (cachedDistanceToT == null)
+                CacheDistanceToT();
+
+            //TODO: Maybe don't need a sorted list. Use a tuple in a list and find any value above distance. Use this and find the previous then lerp between them.
+
+            return 0;
+        }
+
+        public float CurveLength()
+        {
+            if (cachedDistanceToT == null)
+                CacheDistanceToT();
+            return cachedDistanceToT.Keys.Last();
+        }
+
+        public Vector3 GetPoint(float t)
+        {
+            int i;
+            if (t >= 1f)
+            {
+                t = 1f;
+                i = points.Length - 2;
+            }
+            else {
+                t = Mathf.Clamp01(t) * CurveCount;
+                i = (int)t;
+                t -= i;
+            }
+            return transform.TransformPoint(Bezier.GetPoint(points[i].curvePoint, points[i].curvePoint + points[i].curveTangent, points[i+1].curvePoint - points[i+1].curveTangent, points[i+1].curvePoint, t));
+        }
+
+        public Vector3 GetVelocity(float t)
+        {
+            int i;
+            if (t >= 1f)
+            {
+                t = 1f;
+                i = points.Length - 2;
+            }
+            else {
+                t = Mathf.Clamp01(t) * CurveCount;
+                i = (int)t;
+                t -= i;
+            }
+            return transform.TransformPoint(Bezier.GetFirstDerivative(points[i].curvePoint, points[i].curvePoint + points[i].curveTangent, points[i+1].curvePoint - points[i+1].curveTangent, points[i+1].curvePoint, t)) -
+                transform.position;
+        }
+
+        public Vector3 GetDirection(float t)
+        {
+            return GetVelocity(t).normalized;
+        }
+
+        public Vector3 GetNormal(float t)
+        {
+            Vector3 tangent = GetDirection(t);
+            Vector3 biNormal = Vector3.Cross(Vector3.up, tangent).normalized;
+            return Vector3.Cross(tangent, biNormal);
+        }
+
+        public CurvePoint NewCurvePoint()
+        {
+            CurvePoint point = points[points.Length - 1];
+            return new CurvePoint(point.curvePoint + point.curveTangent * 0.5f, point.curveTangent);
+        }
+
+        public static class Bezier
+        {
+            public static Vector3 GetPoint(Vector3 p0, Vector3 p1, Vector3 p2, Vector3 p3, float t)
+            {
+                t = Mathf.Clamp01(t);
+                float oneMinusT = 1f - t;
+                return
+                    oneMinusT * oneMinusT * oneMinusT * p0 +
+                    3f * oneMinusT * oneMinusT * t * p1 +
+                    3f * oneMinusT * t * t * p2 +
+                    t * t * t * p3;
+            }
+
+            public static Vector3 GetFirstDerivative(Vector3 p0, Vector3 p1, Vector3 p2, Vector3 p3, float t)
+            {
+                t = Mathf.Clamp01(t);
+                float oneMinusT = 1f - t;
+                return
+                    3f * oneMinusT * oneMinusT * (p1 - p0) +
+                    6f * oneMinusT * t * (p2 - p1) +
+                    3f * t * t * (p3 - p2);
+            }
+
+
+        }
+    }
+}
---
+++ Assets/Plugins/BedrockFramework/Editor/CustomCurve_Editor.cs
@@ -0,0 +1,169 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEditor;
+
+namespace BedrockFramework.CustomLine
+{
+    [CustomEditor(typeof(CustomCurve))]
+    public class CustomCurve_Editor : Editor
+    {
+        private const int stepsPerCurve = 10;
+        private const float handleSize = 0.04f;
+        private const float pickSize = 0.06f;
+
+        private CustomCurve line;
+        private Transform handleTransform;
+        private Quaternion handleRotation;
+        private SerializedProperty linePoints;
+        private int selectedIndex = -1;
+
+
+
+        private void OnEnable()
+        {
+            line = target as CustomCurve;
+        }
+
+        public override void OnInspectorGUI()
+        {
+            EditorGUILayout.LabelField("Length: " + line.CurveLength().ToString());
+            EditorGUILayout.LabelField("Segments: " + line.CurveCount.ToString());
+        }
+
+        private void AddCurvePoint()
+        {
+            linePoints = serializedObject.FindProperty("points");
+            linePoints.InsertArrayElementAtIndex(linePoints.arraySize);
+
+            CustomCurve.CurvePoint newCurvePoint = line.NewCurvePoint();
+
+            SerializedProperty curvePoint = linePoints.GetArrayElementAtIndex(linePoints.arraySize - 1);
+            curvePoint.FindPropertyRelative("curvePoint").vector3Value = newCurvePoint.curvePoint;
+            curvePoint.FindPropertyRelative("curveTangent").vector3Value = newCurvePoint.curveTangent;
+
+            selectedIndex = linePoints.arraySize - 1;
+        }
+
+        private void OnSceneGUI()
+        {
+            // Disable selecting under mouse if we have a selected index.
+            if (Event.current.type == EventType.Layout)
+            {
+                if (selectedIndex != -1)
+                    HandleUtility.AddDefaultControl(0);
+            }
+
+            // Drop the selected index if mouse up over no handle.
+            if (Event.current.type == EventType.MouseUp)
+            {
+                if (GUIUtility.hotControl == 0)
+                    selectedIndex = -1;
+            }
+
+            serializedObject.Update();
+            handleTransform = line.transform;
+            handleRotation = Tools.pivotRotation == PivotRotation.Local ? handleTransform.rotation : Quaternion.identity;
+
+            Handles.color = Color.white;
+
+            linePoints = serializedObject.FindProperty("points");
+
+            EditorGUI.BeginChangeCheck();
+
+            CustomCurve.CurvePoint p0 = ShowPoint(0);
+            for (int i = 1; i < line.points.Length; i += 1)
+            {
+                Handles.zTest = UnityEngine.Rendering.CompareFunction.Always;
+                CustomCurve.CurvePoint p1 = ShowPoint(i);
+
+                Handles.zTest = UnityEngine.Rendering.CompareFunction.LessEqual;
+                Handles.DrawBezier(p0.curvePoint, p1.curvePoint, p0.curvePoint + p0.curveTangent, p1.curvePoint - p1.curveTangent, Color.white, null, 2f);
+
+                Handles.zTest = UnityEngine.Rendering.CompareFunction.Greater;
+                Handles.DrawBezier(p0.curvePoint, p1.curvePoint, p0.curvePoint + p0.curveTangent, p1.curvePoint - p1.curveTangent, Color.grey, null, 2f);
+
+                if (i == linePoints.arraySize - 1)
+                    DrawAddButton(p1);
+
+                p0 = p1;
+            }
+
+            if (EditorGUI.EndChangeCheck() || line.transform.hasChanged)
+            {
+                line.CacheDistanceToT();
+            }
+
+            //ShowDirections();
+
+            serializedObject.ApplyModifiedProperties();
+        }
+
+        void ShowDirections()
+        {
+            Handles.color = Color.green;
+
+            int steps = stepsPerCurve * line.CurveCount;
+            for (int i = 1; i <= steps; i++)
+            {
+                Vector3 lineEnd = line.GetPoint(i / (float)steps);
+                Handles.color = Color.green;
+                Handles.DrawLine(lineEnd, lineEnd + line.GetDirection(i / (float)steps));
+
+                Handles.color = Color.red;
+                Handles.DrawLine(lineEnd, lineEnd + line.GetNormal(i / (float)steps));
+            }
+        }
+
+        private CustomCurve.CurvePoint ShowPoint(int index)
+        {
+            SerializedProperty curvePoint = linePoints.GetArrayElementAtIndex(index);
+            SerializedProperty curvePos = curvePoint.FindPropertyRelative("curvePoint");
+            SerializedProperty curveTangent = curvePoint.FindPropertyRelative("curveTangent");
+
+            bool selected = index == selectedIndex;
+
+            // Position Drawer
+            Vector3 point = handleTransform.TransformPoint(curvePos.vector3Value);
+            float size = HandleUtility.GetHandleSize(point);
+
+            Handles.color = Color.white;
+            if (selected)
+                point = Handles.DoPositionHandle(point, handleRotation);
+            else if (Handles.Button(point, handleRotation, size * handleSize, size * pickSize, Handles.DotHandleCap))
+                selectedIndex = index;
+
+            curvePos.vector3Value = handleTransform.InverseTransformPoint(point);
+
+            // Tangent Drawer
+            Vector3 tangent = handleTransform.TransformVector(curveTangent.vector3Value) + point;
+            if (selected)
+                tangent = Handles.FreeMoveHandle(tangent, handleRotation, 0.5f, Vector3.one, Handles.CircleHandleCap);
+            tangent -= point;
+            curveTangent.vector3Value = handleTransform.InverseTransformVector(tangent);
+
+            Handles.color = Color.grey;
+            if (selected)
+                Handles.DrawLine(point, point + tangent);
+
+            return new CustomCurve.CurvePoint(point, tangent);
+        }
+
+        private void DrawAddButton(CustomCurve.CurvePoint curvePoint)
+        {
+            Handles.BeginGUI();
+
+            //float size = HandleUtility.GetHandleSize(curvePoint.curvePoint + curvePoint.curveTangent.normalized);
+            Vector3 screenPoint = Camera.current.WorldToScreenPoint(curvePoint.curvePoint + curvePoint.curveTangent * 0.5f);
+
+
+            if (GUI.Button(new Rect(screenPoint.x - 10, Screen.height - screenPoint.y - 50, 20, 20), "+"))
+                AddCurvePoint();
+
+            Handles.EndGUI();
+        }
+    }
+}
+
+
+
